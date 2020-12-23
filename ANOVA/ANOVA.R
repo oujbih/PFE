74aeb8a7e5c55e09f3d8101dfa8d83210e830186
abderrahim oujbih <30387201+oujbih@users.noreply.github.com>
2020-12-23 08:29:46 -0100

    Update ANOVA.R

--- ANOVA/ANOVA.R
+++ ANOVA/ANOVA.R
@@ -23,21 +23,35 @@ summary(InsectSprays)
 plot(spray,count,col=rainbow(6))
 
 #Anova
-# transformer la variable  X^2
+# transformer la variable  X^2  because X don't have a normal distribution 
+shapiro.test(count)      # p < 0.05
+hist(count, xlim=c(0,30), col=rgb(0,1,0,0.5),density = 30,probability = TRUE)
+lines(density(count),col=2) 
+
 new.count <- sqrt(count)
 tapply(new.count, spray, mean) 
 tapply(new.count, spray, var)
 plot(spray,new.count)
-par(mfrow=c(1,1))
-hist(count, xlim=c(0,30), col=rgb(0,1,0,0.5),density = 30)
-hist(new.count, xlim=c(0,6), col=rgb(1,0,0,0.5),density = 30)
+par(mfrow=c(1,2))
+hist(count, xlim=c(0,30), col=rgb(0,1,0,0.5),density = 30,probability = TRUE)
+lines(density(count),col=2) 
+hist(new.count, xlim=c(0,6), col=rgb(1,0,0,0.5),density = 30, probability = TRUE)
+lines(density(new.count),col=2) 
+shapiro.test(new.count)      # p > 0.05
 
-#Model 
+
+# normal Q-Q plot 
+par(mfrow=c(1,2))
+qqnorm(count,main="QQ plof From X")
+qqline(count,col="red")
+
+qqnorm(new.count,main="QQ plof From X^2")
+qqline(new.count,col="red")
+
+#Model avona
 modele <- aov(new.count ~ spray) 
 coef(modele)
 anova(modele)
 summary(modele)
 TukeyHSD(modele) 
 plot(modele)
-
-
